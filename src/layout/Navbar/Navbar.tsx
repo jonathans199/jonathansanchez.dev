e01541de1814d9006eb4264c0249558af9954e8e
jonathans199 <jso1919@gmail.com>
2023-04-05 12:22:54 -0400

    updated resume and home page

--- src/layout/Navbar/Navbar.tsx
+++ src/layout/Navbar/Navbar.tsx
@@ -3,144 +3,166 @@ import Image from 'next/image'
 
 import Link from 'next/link'
 
-import { FaBars, FaTimes, FaGithub, FaLinkedin, FaYoutube, FaMedium } from 'react-icons/fa'
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaYoutube, FaMedium, FaNpm, FaStackOverflow } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 export const Navbar = () => {
-  const [nav, setNav] = React.useState(false)
+	const [nav, setNav] = React.useState(false)
 
-  const handleMenuToggle = () => setNav(!nav)
+	const handleMenuToggle = () => setNav(!nav)
 
-  return (
-    <div className='fixed w-full h[80px] flex justify-between items-center p-4 bg-black text-gray-100 z-50'>
-      <Link href='/' className='hover:cursor-pointer hover:scale-110 duration-500 flex items-center'>
-        <Image
-          className='bg-slate-200 rounded-full'
-          src='/jons-logo.png'
-          alt='jonathanSanchez.dev'
-          width={50}
-          height={37}
-          priority
-        />
-        <h2 className='ml-1 text-gray-400 text-2xl tracking-widest font-medium'>.JS</h2>
-      </Link>
+	return (
+		<div className='fixed w-full h[80px] flex justify-between items-center p-4 bg-black text-gray-100 z-50'>
+			<Link href='/' className='hover:cursor-pointer hover:scale-110 duration-500 flex items-center'>
+				<Image
+					className='bg-slate-200 rounded-full'
+					src='/jons-logo.png'
+					alt='jonathanSanchez.dev'
+					width={50}
+					height={37}
+					priority
+				/>
+				<h2 className='ml-1 text-gray-400 text-2xl tracking-widest font-medium'>.JS</h2>
+			</Link>
 
-      {/* hamburger  */}
-      <div onClick={handleMenuToggle} className='z-10'>
-        {!nav ? (
-          <FaBars size={25} className=' text-slate-500 hover:text-red-600 hover:scale-110 duration-500 cursor-pointer' />
-        ) : (
-          <FaTimes size={20} className='hover:text-red-600 duration-300 cursor-pointer' />
-        )}
-      </div>
+			{/* hamburger  */}
+			<div onClick={handleMenuToggle} className='z-10'>
+				{!nav ? (
+					<FaBars
+						size={25}
+						className=' text-slate-500 hover:text-red-600 hover:scale-110 duration-500 cursor-pointer'
+					/>
+				) : (
+					<FaTimes size={20} className='hover:text-red-600 duration-300 cursor-pointer' />
+				)}
+			</div>
 
-      {/* Menu */}
-      <div
-        className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col items-center pt-40'}>
-        <ul>
-          <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
-            <Link href='/#top' onClick={() => setNav(false)}>
-              HOME
-            </Link>
-          </li>
-          <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
-            <Link href='/#about' onClick={() => setNav(false)}>
-              ABOUT
-            </Link>
-          </li>
-          <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
-            <Link href='/#skills' onClick={() => setNav(false)}>
-              SKILLS
-            </Link>
-          </li>
-          <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
-            <Link href='/work' onClick={() => setNav(false)}>
-              WORK
-            </Link>
-          </li>
-          <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
-            <Link href='tutorials' onClick={() => setNav(false)}>
-              TUTORIALS
-            </Link>
-          </li>
-          {/* <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
+			{/* Menu */}
+			<div
+				className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col items-center pt-40'}>
+				<ul>
+					<li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
+						<Link href='/#top' onClick={() => setNav(false)}>
+							HOME
+						</Link>
+					</li>
+					<li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
+						<Link href='/#about' onClick={() => setNav(false)}>
+							ABOUT
+						</Link>
+					</li>
+					<li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
+						<Link href='/#skills' onClick={() => setNav(false)}>
+							SKILLS
+						</Link>
+					</li>
+					<li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
+						<Link href='/work' onClick={() => setNav(false)}>
+							WORK
+						</Link>
+					</li>
+					<li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
+						<Link href='tutorials' onClick={() => setNav(false)}>
+							TUTORIALS
+						</Link>
+					</li>
+					{/* <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
             <Link href='contributions' onClick={() => setNav(false)}>
               CONTRIBUTIONS
             </Link>
           </li> */}
-          <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
-            <Link href='talks' onClick={() => setNav(false)}>
-              PRESENTATIONS
-            </Link>
-          </li>
-          <li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
-            <Link href='/#contact' onClick={() => setNav(false)}>
-              CONTACT ME
-            </Link>
-          </li>
-        </ul>
-      </div>
+					<li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
+						<Link href='talks' onClick={() => setNav(false)}>
+							PRESENTATIONS
+						</Link>
+					</li>
+					<li className='py-6 text-4xl hover:text-red-600 hover:ml-[-10px] duration-300'>
+						<Link href='/#contact' onClick={() => setNav(false)}>
+							CONTACT ME
+						</Link>
+					</li>
+				</ul>
+			</div>
 
-      {/* social icons  */}
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-        <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
-            <a
-              href='https://www.linkedin.com/in/jonathansanchez199/'
-              target='_blank'
-              className='flex justify-between items-center w-full text-gray-500'
-              rel='noreferrer'>
-              LinkedIn <FaLinkedin size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
-            <a
-              href='https://github.com/jonathans199'
-              className='flex justify-between items-center w-full text-gray-500'
-              rel='noreferrer'
-              target='_blank'>
-              Github <FaGithub size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
-            <a
-              href='https://www.youtube.com/@jonsthewebguy'
-              className='flex justify-between items-center w-full text-gray-500'
-              rel='noreferrer'
-              target='_blank'>
-              YouTube <FaYoutube size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
-            <a
-              href='https://medium.com/@jonathans199'
-              className='flex justify-between items-center w-full text-gray-500'
-              rel='noreferrer'
-              target='_blank'>
-              Medium <FaMedium size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
-            <a
-              href='mailto:jonathans199@gmail.com'
-              className='flex justify-between items-center w-full text-gray-500'
-              rel='noreferrer'
-              target='_blank'>
-              Email <HiOutlineMail size={30} />
-            </a>
-          </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
-            <a
-              href='/JonathanSanchez-FullStack-and-Mobile-SoftwareEngineer-Resume-2023.pdf'
-              className='flex justify-between items-center w-full text-gray-500'
-              rel='noreferrer'
-              target='_blank'>
-              Resume <BsFillPersonLinesFill size={30} />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  )
+			{/* social icons  */}
+			<div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+				<ul>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
+						<a
+							href='https://www.linkedin.com/in/jonathansanchez199/'
+							target='_blank'
+							className='flex justify-between items-center w-full text-gray-500'
+							rel='noreferrer'>
+							LinkedIn <FaLinkedin size={30} />
+						</a>
+					</li>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
+						<a
+							href='https://github.com/jonathans199'
+							className='flex justify-between items-center w-full text-gray-500'
+							rel='noreferrer'
+							target='_blank'>
+							Github <FaGithub size={30} />
+						</a>
+					</li>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
+						<a
+							href='https://www.youtube.com/@jonsthewebguy'
+							className='flex justify-between items-center w-full text-gray-500'
+							rel='noreferrer'
+							target='_blank'>
+							YouTube <FaYoutube size={30} />
+						</a>
+					</li>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
+						<a
+							href='https://medium.com/@jonathans199'
+							className='flex justify-between items-center w-full text-gray-500'
+							rel='noreferrer'
+							target='_blank'>
+							Medium <FaMedium size={30} />
+						</a>
+					</li>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
+						<a
+							href='https://stackoverflow.com/users/8698679/jonathan-sanchez'
+							className='flex justify-between items-center w-full text-gray-500'
+							rel='noreferrer'
+							target='_blank'>
+							StackOF
+							<FaStackOverflow size={30} />
+						</a>
+					</li>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
+						<a
+							href='https://www.npmjs.com/~jonathans199'
+							className='flex justify-between items-center w-full text-gray-500'
+							rel='noreferrer'
+							target='_blank'>
+							NPM <FaNpm size={30} />
+						</a>
+					</li>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
+						<a
+							href='mailto:jonathans199@gmail.com'
+							className='flex justify-between items-center w-full text-gray-500'
+							rel='noreferrer'
+							target='_blank'>
+							Email <HiOutlineMail size={30} />
+						</a>
+					</li>
+					<li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-slate-900'>
+						<a
+							href='/JonathanSanchez-FullStack-and-Mobile-SoftwareEngineer-Resume-2023.pdf'
+							className='flex justify-between items-center w-full text-gray-500'
+							rel='noreferrer'
+							target='_blank'>
+							Resume <BsFillPersonLinesFill size={30} />
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	)
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,164 +22,189 @@ import Css from './../components/icons/Css'
 import Aws from '../components/icons/Aws'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Jonathan Sanchez \ FullStack & Mobile Software Engineer</title>
-        <meta name='description' content='Welcome to my portfolio site build with React in Next.js' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Navbar />
-      <div className='max-w-[1200px] mx-auto px-4 h-full'>
-        <section className='h-screen ' id='top'>
-          <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-2xl text-red-600'>Hi my name is</p>
-            <h1 className='text-4xl  sm:text-7xl font-bold text-[#ccd6f6]'>Jonathan Sanchez</h1>
+	return (
+		<>
+			<Head>
+				<title>Jonathan Sanchez \ FullStack & Mobile Software Engineer</title>
+				<meta name='description' content='Welcome to my portfolio site build with React in Next.js' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Navbar />
+			<div className='max-w-[1200px] mx-auto px-4 h-full'>
+				<section className='h-screen ' id='top'>
+					<div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+						<p className='text-2xl text-red-600'>Hi my name is</p>
+						<h1 className='text-4xl  sm:text-7xl font-bold text-[#ccd6f6]'>Jonathan Sanchez</h1>
 
-            <h2 className='text-2xl sm:text-5xl font-bold text-[#8892b0]'>
-              <TypeAnimation
-                sequence={[
-                  'Full-Stack Developer 👨‍💻',
-                  2000,
-                  'Girl Dad 👯‍♀️',
-                  2000,
-                  'Drummer 🥁',
-                  2000,
-                  'Instructor 👨‍🏫',
-                  2000,
-                  'Dog Lover 🐶',
-                  2000,
-                ]}
-                wrapper='div'
-                cursor={true}
-                repeat={Infinity}
-                // style={{ fontSize: '2em' }}
-              />
-            </h2>
-            <p className='text-xl text-[#8892b0] py-4 max-w-[700px]'>
-              As a full-stack and mobile developer based in sunny Florida, I am dedicated to creating modern software
-              solutions with visually appealing and user-friendly UI/UX.
-            </p>
-            <Link href='/work'>
-              <button className='primary-rd-bg text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
-                view work
-                <span className='hover:rotate-90 duration-300'>
-                  <HiArrowNarrowRight className='ml-4' />
-                </span>
-              </button>
-            </Link>
-          </div>
-        </section>
+						<h2 className='text-2xl sm:text-5xl font-bold text-[#8892b0]'>
+							<TypeAnimation
+								sequence={[
+									'Full-Stack Engineer 👨‍💻',
+									2000,
+									'Mobile Software Engineer 📱',
+									2000,
+									'Girl Dad 👯‍♀️',
+									2000,
+									'Believer 🙏',
+									2000,
+									'Drummer 🥁',
+									2000,
+									'Educator 👨‍🏫',
+									2000,
+									'Dog Lover 🐶',
+									2000,
+								]}
+								wrapper='div'
+								cursor={true}
+								repeat={Infinity}
+								// style={{ fontSize: '2em' }}
+							/>
+						</h2>
+						<p className='text-xl text-[#8892b0] py-4 max-w-[700px]'>
+							As a full-stack and mobile developer based in sunny Florida, I am dedicated to creating modern software
+							solutions with visually appealing and user-friendly UI/UX.
+						</p>
+						<Link href='/work'>
+							<button className='primary-rd-bg text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
+								view work
+								<span className='hover:rotate-90 duration-300'>
+									<HiArrowNarrowRight className='ml-4' />
+								</span>
+							</button>
+						</Link>
+					</div>
+				</section>
 
-        <div className='scroll' id='about' />
-        <section className='h-screen min'>
-          <div className='flex flex-col justify-center items-center'>
-            <div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
-              <div className='sm:text-right pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-red-600'>About</p>
-              </div>
-            </div>
-            <div className='max-w-[1200px] text-right w-full grid sm:grid-cols-2 gap-8 px-4'>
-              <div>
-                <p className='text-3xl font-bold'>
-                  <q>Ready Fire, Aim</q>
-                </p>
-                <p>
-                  As a software engineer passionate about good design and UX, I am committed to creating elegant and
-                  user-friendly software solutions. Having a strong background in technical skill and accompanied design
-                  appreciation, I bring a unique perspective to my work that emphasizes the importance of aesthetics and
-                  usability. <br />
-                  <br />
-                  I&lsquo;m a self-starter with a passion for delivering top-notch products that go above and beyond
-                  what people expect. I love figuring out new and innovative ways to make the user experience even
-                  better, whether that&lsquo;s by tweaking the design or coming up with clever solutions to tough
-                  problems.
-                  <br />
-                  <br />
-                  Specialties: Typescript, Javascript, React, React Native, Node.js, Next.js, Mongo, AWS, CSS, HTML
-                </p>
-              </div>
-              <img src='/img/jonathan.jpeg' alt='' />
-            </div>
-          </div>
-        </section>
+				<div className='scroll' id='about' />
+				<section className='h-screen min'>
+					<div className='flex flex-col justify-center items-center'>
+						<div className='max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8'>
+							<div className='sm:text-right pb-8'>
+								<p className='text-4xl font-bold inline border-b-4 border-red-600'>About</p>
+							</div>
+						</div>
+						<div className='max-w-[1200px] text-right w-full grid sm:grid-cols-2 gap-8 px-4'>
+							<div>
+								<p className='text-3xl font-bold'>
+									<q>Ready Fire, Aim</q>
+								</p>
+								<p>
+									As a software engineer passionate about good design and UX, I am committed to creating elegant and
+									user-friendly software solutions. Having a strong background in technical skill and accompanied design
+									appreciation, I bring a unique perspective to my work that emphasizes the importance of aesthetics and
+									usability. <br />
+									<br />
+									I&lsquo;m a self-starter with a passion for delivering top-notch products that go above and beyond
+									what people expect. I love figuring out new and innovative ways to make the user experience even
+									better, whether that&lsquo;s by tweaking the design or coming up with clever solutions to tough
+									problems.
+									<br />
+									<br />
+									Specialties: Typescript, Javascript, React, React Native, Node.js, Next.js, Mongo, AWS, CSS, HTML
+								</p>
+							</div>
+							<img src='/img/jonathan.jpeg' alt='' />
+						</div>
+					</div>
+				</section>
 
-        <div className='scroll' id='work' />
-        <div className=''>
-          <div className='flex flex-col justify-center'>
-            <h2 className='text-4xl font-bold inline border-b-4 text-gray-300 border-red-600'>Work</h2>
-            <p>checkout some of my work </p>
-          </div>
-          <section className='max-w-[1200px] mx-auto'>
-            {projects.slice(0, 3).map(item => (
-              <WorkItem key={item.id} item={item} />
-            ))}
-          </section>
-          <Link href='/work'>
-            <button className='primary-rd-bg text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
-              View More work
-              <span className='hover:rotate-90 duration-300'>
-                <HiArrowNarrowRight className='ml-4' />
-              </span>
-            </button>
-          </Link>
-        </div>
+				<div className='scroll' id='work' />
+				<div className=''>
+					<div className='flex flex-col justify-center'>
+						<h2 className='text-4xl font-bold inline border-b-4 text-gray-300 border-red-600'>Work</h2>
+						<p>checkout some of my work </p>
+					</div>
+					<section className='max-w-[1200px] mx-auto'>
+						{projects.slice(0, 3).map(item => (
+							<WorkItem key={item.id} item={item} />
+						))}
+					</section>
+					<Link href='/work'>
+						<button className='primary-rd-bg text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
+							View More work
+							<span className='hover:rotate-90 duration-300'>
+								<HiArrowNarrowRight className='ml-4' />
+							</span>
+						</button>
+					</Link>
+				</div>
 
-        <div className='scroll' id='skills' />
-        <section className='h-screen min-h-screen'>
-          <div className='flex flex-col  justify-center'>
-            <h2 className='text-4xl font-bold inline border-b-4 mb-4 border-red-600'>Skills</h2>
-          </div>
-          <div className='grid grid-cols-3 sm:grid-cols-4 gap-20 text-center my-8'>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={Javascript} title='Javascript' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={ReactIcon} title='React' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={Css} title='Css' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={Nextjs} title='Next.js' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={Html} title='Html' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={Sass} title='Sass' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={Node} title='Node' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={MongoDb} title='MongoDb' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={Tailwind} title='Tailwind' />
-            </div>
-            <div className='hover:scale-110 duration500'>
-              <Icon IconType={Aws} title='Aws' />
-            </div>
-          </div>
-        </section>
+				<div className='scroll' id='skills' />
+				<section className='h-screen min-h-screen'>
+					<div className='flex flex-col  justify-center'>
+						<h2 className='text-4xl font-bold inline border-b-4 mb-4 border-red-600'>Skills</h2>
+					</div>
+					<div className='grid grid-cols-3 sm:grid-cols-4 gap-20 text-center my-8'>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={Javascript} title='Javascript' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={ReactIcon} title='React' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={Css} title='Css' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={Nextjs} title='Next.js' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={Html} title='Html' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={Sass} title='Sass' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={Node} title='Node' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={MongoDb} title='MongoDb' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={Tailwind} title='Tailwind' />
+						</div>
+						<div className='hover:scale-110 duration500'>
+							<Icon IconType={Aws} title='Aws' />
+						</div>
+					</div>
+				</section>
+				{/* 
+        <div className='scroll' id='collaboration' />
+				<div className=''>
+					<div className='flex flex-col justify-center'>
+						<h2 className='text-4xl font-bold inline border-b-4 text-gray-300 border-red-600'>Collaborations</h2>
+						<p>checkout some of my work </p>
+					</div>
+					<section className='max-w-[1200px] mx-auto'>
+						{projects.slice(0, 3).map(item => (
+							<WorkItem key={item.id} item={item} />
+						))}
+					</section>
+					<Link href='/work'>
+						<button className='primary-rd-bg text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-red-600 hover:border-red-600'>
+							View More work
+							<span className='hover:rotate-90 duration-300'>
+								<HiArrowNarrowRight className='ml-4' />
+							</span>
+						</button>
+					</Link>
+				</div> */}
 
-        <div id='contact' className='w-full h-screen flex justify-center items-center p-4'>
-          <div className='h-screen '>
-            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-              <h2 className='text-4xl font-bold inline border-b-4 border-red-600 mb-4'>Contact</h2>
-              <p className='text-xl text-[#8892b0] py-4 max-w-[700px]'>
-                Lets collaborate on your project and change the world one screen at a time.
-              </p>
+				<div id='contact' className='w-full h-screen flex justify-center items-center p-4'>
+					<div className='h-screen '>
+						<div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+							<h2 className='text-4xl font-bold inline border-b-4 border-red-600 mb-4'>Contact</h2>
+							<p className='text-xl text-[#8892b0] py-4 max-w-[700px]'>
+								Lets collaborate on your project and change the world one screen at a time.
+							</p>
 
-              <a href='mailto:jonathans199@gmail.com'>
-                <h2 className='text-2xl sm:text-2xl font-bold text-[#8892b0]'>jonathans199@gmail.com</h2>
-              </a>
-            </div>
-          </div>
+							<a href='mailto:jonathans199@gmail.com'>
+								<h2 className='text-2xl sm:text-2xl font-bold text-[#8892b0]'>jonathans199@gmail.com</h2>
+							</a>
+						</div>
+					</div>
 
-          {/* <form action='' className='flex flex-col max-w-[600px] w-full'>
+					{/* <form action='' className='flex flex-col max-w-[600px] w-full'>
             <div
               className='pb-8
             '>
@@ -198,8 +223,8 @@ export default function Home() {
               Lets Collaborate
             </button>
           </form> */}
-        </div>
-      </div>
-    </>
-  )
+				</div>
+			</div>
+		</>
+	)
 }
